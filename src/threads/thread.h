8d42f05e0a5ffc9c9313a18920ebace37e5e018b
xliu72 <724718356@qq.com>
2019-09-29 13:38:19 -0400

    fixed bug.

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -93,6 +93,9 @@ struct thread
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
 
+    int64_t wake_time;         /* the time when it should be woke up*/
+
+
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir;                  /* Page directory. */
